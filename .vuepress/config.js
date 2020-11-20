b14c1d717f37e6f9c8930f60cbaae2aa8a95ce61
Christopher Gammie <contact@gammie.co.uk>
2020-11-20 16:54:37 +0000

    Initial structure

---
+++ .vuepress/config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Laravel JSON API',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: 'Build JSON:API compliant APIs in Laravel.',
+  base: "/docs/",
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    searchPlaceholder: 'Search...',
+    lastUpdated: 'Last Updated',
+    sidebarDepth: 0,
+    nav: [
+      {
+        text: "Home",
+        link: "https://laraveljsonapi.io"
+      },
+      {
+        text: "Version",
+        link: "/",
+        items: [{ text: "1.0", link: "/1.0/" }],
+      }
+    ],
+    sidebar: {
+      "/1.0/": require("./1.0"),
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Laravel JSON API Documentation
